a615676b8d375aede517eabed3b6d71ab16de6fb
vadik184 <133372327+vadik184@users.noreply.github.com>
2024-07-11 22:56:54 +0300

    update 1

---
+++ js/array.js
@@ -0,0 +1,51 @@
+// Завдання 1: У фразі "JavaScript is a versatile language" треба переставити два перші слова в кінець,
+//щоб вийшла фраза "a versatile language JavaScript is"
+
+// const sentence = "JavaScript is a versatile language";
+// console.log(sentence);
+// const array = sentence.split(" ");
+// const firstWord = array.shift();
+// array.push(firstWord);
+// console.log(array.join(" "));
+
+// const sentence = "JavaScript is a versatile language";
+// console.log(sentence);
+// const array = sentence.split(" ");
+// const firstWord = array.splice(0, 2);
+// array.push(firstWord.join(" "));
+// console.log(array.join(" "));
+
+const sentence = "JavaScript is a versatile language";
+console.log(sentence);
+const array = sentence
+  .split(" ")
+  .splice(2)
+  .concat(sentence.split(" ").splice(0, 2))
+  .join(" ");
+console.log(array);
+
+// завдання 2: відсортвати масив за зростанням
+
+const numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+
+function sortNumber(array) {
+  return array.sort((a, b) => a - b);
+}
+function sortNumber2(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+}
+console.log(numbers);
+let sortNumb2 = sortNumber2(numbers);
+let sortNumb = sortNumber(numbers);
+
+console.log(sortNumb);
+console.log(sortNumb2);
